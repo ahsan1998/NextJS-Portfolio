3719438694956db6eb0f0a2acf409864bcaec258
Ahsan <mahsan1998@gmail.com>
2024-06-21 16:20:16 +0500

    Minor Bug Fixes

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-2xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-lg sm:text-3xl  text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
-              Hello, I'm Ahsan
+            Hello, I&apos;m{" "}Ahsan
             </span>
             <br></br>
             <TypeAnimation
